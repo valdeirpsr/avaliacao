b9f7113849f1b7fab30019d577a63abeb20b0f38
Valdeir S <valdeirpsr@hotmail.com.br>
2023-08-13 07:39:18 -0300

    chore: adiciona suporte ao tailwindcss
    Ele ajudará a criar a estilização do CSS, além de padronizar e
    normalizar os elementos.

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.ts",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+    container: {
+      padding: '2rem',
+    },
+  },
+  plugins: [],
+}
