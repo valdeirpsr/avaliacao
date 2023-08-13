229f75ac4ff4b969bb8307c77ba452080decf3b9
Valdeir S <valdeirpsr@hotmail.com.br>
2023-08-13 08:56:38 -0300

    feat: adiciona o componente PsrHeader
    Ele poderá exibir, no topo da tela, a logo, menus etc.

---
+++ resources/js/Components/PsrHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header"></div>
+</template>
+
+<style scoped>
+.header {
+  @apply sticky
+  top-0
+  z-10
+  flex
+  h-16
+  items-center
+  justify-between
+  space-x-12
+  bg-purple-600
+  bg-opacity-75
+  px-4
+  backdrop-blur
+}
+</style>
