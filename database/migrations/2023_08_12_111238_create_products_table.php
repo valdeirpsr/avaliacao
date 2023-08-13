ec16bade4a401d99572af618118bf10e76daabd4
Valdeir S <valdeirpsr@hotmail.com.br>
2023-08-13 07:21:02 -0300

    chore: cria estrutura da tabela products
    A tabela contém a seguinte estrutura:
    
    | Coluna       | Descrição                |
    |--------------|--------------------------|
    | id           | Identificação do produto |
    | name         | Nome do Produto          |
    | description  | Descrição do produto     |
    | voltage      | Voltagem do produto      |
    | manufacturer | Fabricante do produto*   |
    
    * Valores possíveis:
    
     - Electrolux
     - Brastemp
     - Fischer
     - Samsung
     - LG

---
+++ database/migrations/2023_08_12_111238_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->text('description');
+            $table->string('voltage');
+            $table->enum('manufacturer', ['Electrolux', 'Brastemp', 'Fischer', 'Samsung', 'LG']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
